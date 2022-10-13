95b69ea3dfdae545c70baf623f7dbbffff6d6833
lVirrueta <luis510vg@gmail.com>
2022-10-13 13:21:42 -0500

    feature/back/ih-structure: proxy structure added

---
+++ api-supermarket/api-gateway/src/common/proxy/client-proxy.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { ClientProxy, ClientProxyFactory, Transport } from "@nestjs/microservices";
+import { RabbitMQ } from "./constants";
+
+@Injectable()
+export class ClientProxySupermarket {
+  constructor(private readonly config: ConfigService) { }
+
+  /* proxy of module admin */
+  clientProxyAdmin(): ClientProxy {
+    return ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: this.config.get('AMQP_URL'),
+        queue: RabbitMQ.AdminQueue,
+      }
+    })
+  }
+
+  /* proxy of module manager */
+  clientProxyManager(): ClientProxy {
+    return ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: this.config.get('AMQP_URL'),
+        queue: RabbitMQ.ManagerQueue,
+      }
+    })
+  }
+
+}
---
+++ api-supermarket/api-gateway/src/common/proxy/constants.ts
@@ -0,0 +1,20 @@
+export enum RabbitMQ {
+  AdminQueue = 'adminQueue',
+  ManagerQueue = 'managerQueue',
+}
+
+/* AdminModule Constants */
+export enum AdminManagerMSG {
+  GET_ONE = 'GET_ONE',
+  GET_ALL = 'GET_ALL',
+  CREATE = 'CREATE',
+  UPDATE = 'UPDATE',
+  DELETE = 'DELETE',
+}
+
+export enum AdminSupermarketMSG {
+  GET_ONE = 'GET_ONE',
+  GET_ALL = 'GET_ALL',
+}
+/* END AdminModule Constants */
+
