36566d96d9d636ec1243b4c24deab180fc45bb76
lVirrueta <luis510vg@gmail.com>
2022-10-13 14:19:05 -0500

    feature/back/ih-structure: manager proxy added

--- api-supermarket/api-gateway/src/modules/manager/manager.module.ts
+++ api-supermarket/api-gateway/src/modules/manager/manager.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { SupermarketController } from './supermarket/supermarket.controller';
 import { DepartmentController } from './department/department.controller';
 import { EmployeeController } from './employee/employee.controller';
+import { ProxyModule } from 'src/common/proxy/proxy.module';
 
 @Module({
+  imports: [ProxyModule],
   controllers: [SupermarketController, DepartmentController, EmployeeController]
 })
 export class ManagerModule {}
