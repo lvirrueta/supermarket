d1568ce94c5b1a93e014816b201527c83510ab9d
lVirrueta <luis510vg@gmail.com>
2022-10-14 13:40:51 -0500

    feature/back/ih-structure: ms admin controller & service

--- api-supermarket/ms-admin/src/app.module.ts
+++ api-supermarket/ms-admin/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ManagerController } from './manager/manager.controller';
+import { ManagerService } from './manager/manager.service';
+import { SupermarketController } from './supermarket/supermarket.controller';
+import { SupermarketService } from './supermarket/supermarket.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [ ManagerController, SupermarketController ],
+  providers: [ ManagerService, SupermarketService ],
 })
 export class AppModule {}
