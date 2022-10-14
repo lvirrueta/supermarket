7eb0b5cfae20c47a84adf00ece8ad6514e88524b
lVirrueta <luis510vg@gmail.com>
2022-10-14 13:47:49 -0500

    feature/back/ih-structure: ms manager controller & service

--- api-supermarket/ms-manager/src/app.module.ts
+++ api-supermarket/ms-manager/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DepartmentController } from './department/department.controller';
+import { DepartmentService } from './department/department.service';
+import { EmployeeController } from './employee/employee.controller';
+import { EmployeeService } from './employee/employee.service';
+import { SupermarketController } from './supermarket/supermarket.controller';
+import { SupermarketService } from './supermarket/supermarket.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [ DepartmentController, EmployeeController, SupermarketController ],
+  providers: [ DepartmentService, EmployeeService, SupermarketService ],
 })
 export class AppModule {}
