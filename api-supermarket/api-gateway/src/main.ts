877166bb47cb08d2d9e5a3d5f18c3aa973bba9f0
lVirrueta <luis510vg@gmail.com>
2022-10-13 19:41:20 -0500

    feature/back/ih/structure: swagger added

--- api-supermarket/api-gateway/src/main.ts
+++ api-supermarket/api-gateway/src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalInterceptors(new TimeOutInterceptor());
+  const OPTIONS = new DocumentBuilder()
+    .setTitle('API Supermarket')
+    .setDescription('Supermarket management system')
+    .setVersion('1.0.0')
+    .build();
+  const DOCUMENT = SwaggerModule.createDocument(app, OPTIONS);
+  SwaggerModule.setup('/api/docs', app, DOCUMENT);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
