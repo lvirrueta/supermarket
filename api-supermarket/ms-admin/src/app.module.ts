6a7872754010a0b331718020793267c3953c4952
lVirrueta <luis510vg@gmail.com>
2022-10-15 15:57:30 -0500

    connection to database postgres

--- api-supermarket/ms-admin/src/app.module.ts
+++ api-supermarket/ms-admin/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminModule } from './admin/admin.module';
 
 @Module({
@@ -9,6 +10,15 @@ import { AdminModule } from './admin/admin.module';
       envFilePath: ['.env'],
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PSWD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    })
   ],
 
 })
--- api-supermarket/ms-manager/src/app.module.ts
+++ api-supermarket/ms-manager/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ManagerModule } from './manager/manager.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -9,6 +10,15 @@ import { ManagerModule } from './manager/manager.module';
       envFilePath: ['.env'],
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PSWD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    })
   ],
 
 })
